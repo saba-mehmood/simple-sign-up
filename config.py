cf52e13151fe0bf6b834e2cf5402914c104419d6
saba-mehmood <68803765+saba-mehmood@users.noreply.github.com>
2022-02-04 17:26:58 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+class TestData:
+  
+ EMAIL='avax3115@yopmail.com'
+ FIRST_NAME='Sara'
+ LAST_NAME='Khan'
+ ADDRESS='Ohio US'
+ CITY='Ohio'
+ ZIP_CODE='45698'
+ PHONE_NO='9365247895'
+ OTP='https://avaxdevapi.akru.co/api/user/showOtp/'+EMAIL
+ SSN = '236547852'
